dda81d6d141d21bcc2a5ab2556da748dd4490631
Erick Peirson <erick.peirson@asu.edu>
2016-04-18 08:19:22 -0400

    sll

--- jhb/settings.py
+++ jhb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,3 +168,6 @@ HAYSTACK_CONNECTIONS = {
 }
 
 VOGONWEB = 'http://www.vogonweb.net'
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
